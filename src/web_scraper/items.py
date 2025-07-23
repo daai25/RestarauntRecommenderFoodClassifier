ee755272e62e0418617fa3d108f7b5083274c325
freinin1 <freinin1@students.zhaw.ch>
2025-07-23 10:56:24 -0400

    remove scrapy infrastructure

--- src/web_scraper/items.py
+++
@@ -1,9 +0,0 @@
-# web_scraper/items.py
-import scrapy
-
-class ImageItem(scrapy.Item):
-    """
-    Item class for storing image data scraped from web pages.
-    """
-    image_urls = scrapy.Field()
-    images = scrapy.Field()
--- src/web_scraper/pipelines.py
+++
@@ -1,9 +0,0 @@
-# src/web_scraper/pipelines.py
-import os
-import hashlib
-import scrapy
-from scrapy.pipelines.images import ImagesPipeline
-
-class CustomImagePipeline(ImagesPipeline):
-    def file_path(self, request, response=None, info=None, *, item=None):
-        return request.url.split("/")[-1]
